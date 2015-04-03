7aa5cdbd08178455ae7c25c63c5a3afd62f63774
Dmitriy Khomitskiy <dkhomits@kcg.com>
2015-04-03 17:01:01 -0500

    First commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var signOutputLabel: UILabel!
+    @IBOutlet weak var outputLabel: UILabel!
+    var num : Double = 0
+    var isClear: Bool = true
+    var sign : Signs? = nil
+    
+    enum Signs: String {
+        case Plus = "+"
+        case Minis = "-"
+        case Divide = "/"
+        case Multiply = "*"
+    }
+    
+    @IBAction func numberAction(sender: UIButton) {
+        if isClear {
+            outToLabel(sender.titleLabel!.text!, sign)
+            isClear = false
+        } else {
+            outToLabel(outputLabel.text! + sender.titleLabel!.text!, sign)
+        }
+    }
+    
+    @IBAction func commandAction(sender: UIButton) {
+        if let tmpSign = sign {
+            num = doWork(num, (outputLabel.text! as NSString).doubleValue, tmpSign)
+        }
+        
+        if Signs.Plus.rawValue == sender.titleLabel!.text! {
+            sign=Signs.Plus
+        }
+        else if(Signs.Munis.rawValue == sender.titleLabel!.text!) {
+            sign=Signs.Munis
+        }
+        else if(Signs.Divide.rawValue == sender.titleLabel!.text!) {
+            sign=Signs.Divide
+        }
+        else if(Signs.Multiply.rawValue == sender.titleLabel!.text!) {
+            sign=Signs.Multiply
+        }
+        
+        isClear = true
+        num = outputLabel.text!.toInt()!
+        signOutputLabel.text! = sender.titleLabel!.text!
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        num = 0
+        isClear = true
+        sign = nil
+        self.outToLabel("0", sign:Signs.Plus)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func outToLabel(numValue: String, sign:Signs?) {
+        outputLabel.text! = numValue
+        signOutputLabel.text! = "\(sign?.rawValue)"
     }
-
-
+    
+    func doWork(num1:Double, num2:Double, sign:Signs) -> Double {
+        switch(sign) {
+        case .Plus:
+            return num1 + num2
+        case .Minis:
+            return num1 - num2
+        case .Divide:
+            return num1 / num2
+        case .Multiply:
+            return num1 * num2
+        }
+    }
 }
-
